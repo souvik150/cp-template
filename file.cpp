fc346e839b585364216bfebb61bab1df28ada5cd
Souvik Mukherjee <90630641+souvik150@users.noreply.github.com>
2024-08-17 13:33:22 +0530

    feat: adds c++ template for competitive coding

---
+++ file.cpp
@@ -0,0 +1,150 @@
+// Author: Souvik Mukherjee
+// Linkedin: https://www.linkedin.com/in/souvik150/
+// Codeforces: https://codeforces.com/profile/souvik150
+// Leetcode: https://leetcode.com/souvik150/
+// Codechef: https://www.codechef.com/users/souvik150
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using namespace std::chrono;
+ 
+/*
+================================ TEMPLATE BY ===================================
+=================================================================================
+=================================================================================
+=================================================================================
+   _____                   _ __      __  ___      __   __              _
+  / ___/____  __  ___   __(_) /__   /  |/  /_  __/ /__/ /_  ___  _____(_)__  ___
+  \__ \/ __ \/ / / / | / / / //_/  / /|_/ / / / / //_/ __ \/ _ \/ ___/ / _ \/ _ \
+ ___/ / /_/ / /_/ /| |/ / / ,<    / /  / / /_/ / ,< / / / /  __/ /  / /  __/  __/
+/____/\____/\__,_/ |___/_/_/|_|  /_/  /_/\__,_/_/|_/_/ /_/\___/_/__/ /\___/\___/
+                                                                /___/
+=================================================================================
+=================================================================================
+=================================================================================
+*/
+ 
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+//Macros
+#define IOtext freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define PI (3.141592653589)
+#define M 1000000007
+#define pb push_back
+#define f first
+#define s second
+#define foreach(i, j, k, in) for(int i=j;i<k;i+=in)
+#define rforeach(i, j, k, in) for(int i=j;i>=k;i-=in)
+#define rep(i,j) foreach(i,0,j,1)
+#define rrep(i,j) rforeach(i,j,0,1)
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+#define sz(s) (int)(s.size())
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define inint(x) int x; cin>>x;
+#define inll(x) long long int x; cin>>x;
+#define instr(x) string x; cin>>x;
+#define all(x) x.begin(), x.end()
+#define os(x) cout << x << " ";
+#define out(x) cout << x << endl;
+#define MAX(x) *max_element(all(x))
+#define MIN(x) *min_element(all(x))
+#define SUM(x) accumulate(all(x), 0LL)
+#define COUNT(x,u) count(all(x), u)
+#define B break
+#define C continue
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define pm cout<<"-1"<<endl
+#define ps(x,y) fixed<<setprecision(y)<<x
+
+//Typedef
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef map<int,int> mii;
+typedef set<int> st;
+ 
+// Debugging macros
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+// Operator overloads
+template<typename T> // cin >> vector<T>
+istream& operator>>(istream &istream, vector<T> &v){for (auto &it : v)cin >> it;return istream;}
+template<typename T> // cout << vector<T>
+ostream& operator<<(ostream &ostream, const vector<T> &c) { for (auto &it : c) cout << it << " "; return ostream; }
+template<typename T, typename V> // cout << map<T,T>
+ostream& operator<<(ostream &ostream, const map<T,V> &c) { for (auto &it : c) cout << it.first << " " << it.second<<endl; return ostream; }
+
+
+//Sorting
+bool sorta(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
+bool sortd(const pair<int,int> &a,const pair<int,int> &b){return (a.second > b.second);}
+
+//Bits
+string decToBinary(int n){string s="";int i = 0;while (n > 0) {s =to_string(n % 2)+s;n = n / 2;i++;}return s;}
+ll binaryToDecimal(string n){string num = n;ll dec_value = 0;int base = 1;int len = num.length();for(int i = len - 1; i >= 0; i--){if (num[i] == '1')dec_value += base;base = base * 2;}return dec_value;}
+
+//Check
+bool isPrime(ll n){if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)return false;for(int i=5;i*i<=n;i=i+6)if(n%i==0||n%(i+2)==0)return false;return true;}
+bool isPowerOfTwo(int n){if(n==0)return false;return (ceil(log2(n)) == floor(log2(n)));}
+bool isPerfectSquare(ll x){if (x >= 0) {ll sr = sqrt(x);return (sr * sr == x);}return false;}
+
+//Constants
+vector <ll> primes;
+vector <bool> is_prime;
+
+// Mathematical functions
+void Sieve(int n){ is_prime.assign(n + 1, true); is_prime[0] = is_prime[1] = false; for(ll i = 2; i * i <= n; i++) if(is_prime[i]) for(ll j = i * i; j <= n; j += i) is_prime[j] = false;}
+void get_primes(int n){ for(int i = 2; i <= n; i++)  if(is_prime[i])  primes.push_back(i); }
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
+ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
+ll moduloMultiplication(ll a,ll b,ll mod){ll res = 0;a %= mod;while (b){if (b & 1)res = (res + a) % mod;b >>= 1;}return res;}
+ll powermod(ll x, ll y, ll p){ll res = 1;x = x % p;if (x == 0) return 0;while (y > 0){if (y & 1)res = (res*x) % p;y = y>>1;x = (x*x) % p;}return res;}
+ll modinv(ll p,ll q){ll ex;ex=M-2;while (ex) {if (ex & 1) {p = (p * q) % M;}q = (q * q) % M;ex>>= 1;}return p;}
+
+ 
+// Timer macros
+#define START_TIMER auto start = high_resolution_clock::now();
+#define END_TIMER                                            \
+  auto stop = high_resolution_clock::now();                  \
+  auto duration = duration_cast<microseconds>(stop - start); \
+  cerr << "Time taken by function: " << duration.count() << " microseconds" << endl;
+
+void solve(){
+  
+}
+
+signed main() {
+ 
+  fastio();
+  START_TIMER
+ 
+  // Main solver function
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+ 
+  END_TIMER
+ 
+  return 0;
+}
